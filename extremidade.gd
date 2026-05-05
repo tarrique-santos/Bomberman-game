67ea67df2f4692905c4cc6911e7706810dd9031c
MoliverZ <ataliba86.ane.k@sou.faccat.br>
2026-05-05 17:26:58 -0300

    mensagem da atualização

--- extremidade.gd
+++ extremidade.gd
@@ -2,8 +2,7 @@ extends TileMapLayer
 
 func _ready():
 	for pos in get_used_cells():
-		var inst = load("res://objects/parede.tscn").instantiate()
+		var inst = load("res://extremidade123.tscn").instantiate()
 		add_child(inst)
 		inst.position = map_to_local(pos) + Vector2(tile_set.tile_size) / 2
 	
-	clear()
